c0855ec54b6e66dab79b97ec77b48cb1ca79fd85
T4N4Y25 <164175722+T4N4Y25@users.noreply.github.com>
2024-04-22 19:52:10 +0200

    Update Player2Movement.cs
    Same changes as Player1

--- Player2Movement.cs
+++ Player2Movement.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Player2Movement : MonoBehaviour
+public class Player2Movement : MonoBehaviour //Same as player one except the projectile mechanic is not included
 {
     private GameObject Player2;
     private Rigidbody2D rb2;
 
     Keyboard key = Keyboard.current;
 
-    public float JumpHeight = 2f;
+    public float JumpHeight = 2f;  //Adjust these to be the same as player1
     public float MovementSpeed = 300f;
     public float JumpTimer = 1f;
 
     private bool OnGround;
 
-    public float groundCheckRadius = 0.1f;
-    public LayerMask groundLayer;
-    public bool isGrounded;
+  ////  public float groundCheckRadius = 0.1f;
+///public LayerMask groundLayer;
+  ///  public bool isGrounded;
 
     // Start is called before the first frame update
     void Start()
@@ -73,18 +73,18 @@ public class Player2Movement : MonoBehaviour
 
     }
 
-    void FixedUpdate()
-    {
-        Vector2 position = rb2.position;
-        position.y -= groundCheckRadius;
-        Collider2D hit = Physics2D.OverlapPoint(position, groundLayer);
-        isGrounded = hit != null;
-        if (isGrounded)
-        {
+  //////  void FixedUpdate()
+   //// {
+     //   Vector2 position = rb2.position;
+      ///  position.y -= groundCheckRadius;
+      //  Collider2D hit = Physics2D.OverlapPoint(position, groundLayer);
+     ///   isGrounded = hit != null;
+      //  if (isGrounded)
+      //  {
             // Prevent the object from moving through the platform
-            rb2.velocity = new Vector2(rb2.velocity.x, 0);
-        }
-    }
+      //      rb2.velocity = new Vector2(rb2.velocity.x, 0);
+      //  }
+  //  }
 
     private void OnCollisionExit2D(Collision2D collision)
     {
